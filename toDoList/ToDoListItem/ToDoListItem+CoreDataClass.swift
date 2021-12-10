090dc050707f3b19fae7e52a2f5df27010a61f50
Wanki <faithon6109@naver.com>
2021-12-10 00:04:22 -0800

    Initial Commit

---
+++ toDoList/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  toDoList
+//
+//  Created by 우완기 on 2021/12/08.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
